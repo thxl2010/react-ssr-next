c287988a3e013410d1c968da7b2c39794f7ecc71
Duyb <1255248666@qq.com>
2022-04-21 15:46:24 +0800

    创建页面 & 页面跳转

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,6 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <nav>
+        <Link href="/list">
+          <a>jump to list page</a>
+        </Link>
+      </nav>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -65,5 +71,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
