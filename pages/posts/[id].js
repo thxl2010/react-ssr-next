0d64fdd88e732394421af1594f218664350b924e
Duyb <1255248666@qq.com>
2022-04-22 22:55:54 +0800

    基于动态路由的静态生成 getStaticPaths

---
+++ pages/posts/[id].js
@@ -0,0 +1,47 @@
+export default function Post({ data }) {
+  return (
+    <ul>
+      <li>
+        <span>{data.id}</span>
+        <span>{data.title}</span>
+      </li>
+    </ul>
+  );
+}
+
+/**
+ * 预渲染：基于动态路由的静态生成
+ * 返回用户可以访问到的所有路由参数
+ */
+export async function getStaticPaths() {
+  // 此处获取所有用户可以访问的路由参数
+  return {
+    // 返回固定格式的路由参数 id: string
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    //当用户访问的路由参数没有在当前函数中返回时，是否显示404页面 false∶显示 true 不显示
+    fallback: false,
+  };
+}
+
+// 返回路由参数所对应的具体数据
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  let data;
+
+  console.log('getStaticProps [id].js params.id :', id);
+
+  switch (id) {
+    case '1':
+      data = { id: 1, title: 'Hello' };
+      break;
+    case '2':
+      data = { id: 2, title: 'Next' };
+      break;
+    default:
+      data = {};
+  }
+
+  return {
+    props: { data },
+  };
+}
