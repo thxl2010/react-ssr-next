e3ea8b63d3209af4b61e6d5736def885a6b049e1
Duyb <1255248666@qq.com>
2022-04-24 14:42:20 +0800

    header

---
+++ theme/index.js
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react';
+// Global style overrides
+import styles from './styles';
+// Component style overrides
+// ... ./components/
+
+const theme = extendTheme({
+  // config,
+  styles,
+  // colors,
+  // breakpoints,
+  // components,
+});
+console.log('theme :', theme);
+
+export default theme;
---
+++ theme/styles.js
@@ -0,0 +1,17 @@
+/*
+ * Global style overrides - Chakra-UI
+ * @Author: Duyb
+ * @Date: 2022-04-24 14:03:23
+ * @Last Modified by: Duyb
+ * @Last Modified time: 2022-04-24 14:12:07
+ */
+
+const styles = {
+  global: (props) => ({
+    'html, body': {
+      fontSize: '12px',
+    },
+  }),
+};
+
+export default styles;
