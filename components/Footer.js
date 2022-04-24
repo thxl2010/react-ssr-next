3c6f41a7ee9d8bd0d402bbd811b6a587b2908df6
Duyb <1255248666@qq.com>
2022-04-24 16:57:44 +0800

    布局 Layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,15 @@
+import { css } from '@emotion/react';
+
+const footer = css`
+  text-align: center;
+  line-height: 40px;
+  margin-top: 20px;
+`;
+
 export default function Footer() {
   return (
-    <footer>
-      © 2022{' '}
+    <footer css={footer}>
+      © 2022&nbsp;
       <a
         href="https://gitee.com/2010thxl/react-ssr-next/tree/demo-movie"
         target="_blank"
