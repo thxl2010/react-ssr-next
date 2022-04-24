8a8d74d6c71c5762fbfcd91f190f495a92293037
Duyb <1255248666@qq.com>
2022-04-24 17:04:40 +0800

    布局 Layout

--- components/Main.js
+++ components/Main.js
@@ -1,3 +1,9 @@
+import { css } from '@emotion/react';
+
+const main = css`
+  min-height: calc(100vh - 52px - 52px - 60px);
+`;
+
 export default function Main({ children }) {
-  return <main>{children}</main>;
+  return <main css={main}>{children}</main>;
 }
